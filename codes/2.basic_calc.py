0c1b919f725731937dcbcf953714c0b6cf8c1850
99003788 <78849542+99003788@users.noreply.github.com>
2021-03-11 05:52:48 +0530

    added my second code from VSCode in windows

---
+++ codes/2.basic_calc.py
@@ -0,0 +1,8 @@
+a= 10
+b=5
+print("a=",a," ","Type->",type(a)," ","Address->",id(a) )
+print("b=",b," ","Type->",type(b)," ","Address->",id(b) )
+print("a+b=",(a+b)," ","Type->",type(a+b)," ","Address->",id(a+b))
+print("a-b=",(a-b)," ","Type->",type(a-b)," ","Address->",id(a-b))
+print("a*b=",(a*b)," ","Type->",type(a*b)," ","Address->",id(a*b))
+print("a/b=",(a/b)," ","Type->",type(a/b)," ","Address->",id(a/b))
